e6d8c75dc737515b1d122e79ee6295391267e8ac
vianatm <tomas.m.viana@metlife.com>
2026-04-16 09:46:03 -0300

    modified:   teste.py

--- teste.py
+++ teste.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-print('Hello, World!')
+print('Bye, World!')
 
 print('This is a test file for pandas library.')
-df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
-print(df)
+df2 = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+print(df2)
