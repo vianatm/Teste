4f3209da17301d4478750ba2d7222deedce2f556
vianatm <tomas.m.viana@metlife.com>
2026-04-28 16:06:06 -0300

    modified:   teste.py mudança valores

--- teste.py
+++ teste.py
@@ -3,7 +3,7 @@ import pandas as pd
 print('Hello, World!')
 
 print('This is a test file for pandas library.')
-df2 = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+df2 = pd.DataFrame({'A': [1, 5, 3], 'B': [4, 5, 6]})
 print(df2)
-df = pd.DataFrame({'A': [4, 5, 6], 'B': [7, 8, 9]})
+df = pd.DataFrame({'A': [4, 10, 6], 'B': [7, 8, 9]})
 print(df)
